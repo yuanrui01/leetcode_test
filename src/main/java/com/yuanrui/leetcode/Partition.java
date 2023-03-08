e73fcd0b5be01287b6963e0f0601ef572da81af4
yuanrui <xdyrfree@gmail.com>
2023-03-08 13:13:08 +0800

    提交尝试

--- src/main/java/com/yuanrui/leetcode/Partition.java
+++ src/main/java/com/yuanrui/leetcode/Partition.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Partition {
 
     public List<List<String>> partition(String s) {
-        System.out.println("我提交12次！");
+        System.out.println("我提交14次！");
         return null;
     }
 }
