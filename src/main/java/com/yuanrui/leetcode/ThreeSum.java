87c52032a5cf75715b529bbe5a1d55535fb82c13
yuanrui01 <xdyrfree@gmail.com>
2023-03-12 14:17:25 +0800

    15. 三数之和

---
+++ src/main/java/com/yuanrui/leetcode/ThreeSum.java
@@ -0,0 +1,37 @@
+package com.yuanrui.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/12-14:16
+ */
+public class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n - 2; ++i) {
+            int x = nums[i];
+            if (i > 0 && x == nums[i - 1]) continue; // 跳过重复数字
+            if (x + nums[i + 1] + nums[i + 2] > 0) break; // 优化一
+            if (x + nums[n - 2] + nums[n - 1] < 0) continue; // 优化二
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int s = x + nums[j] + nums[k];
+                if (s > 0) --k;
+                else if (s < 0) ++j;
+                else {
+                    ans.add(List.of(x, nums[j], nums[k]));
+                    for (++j; j < k && nums[j] == nums[j - 1]; ++j); // 跳过重复数字
+                    for (--k; k > j && nums[k] == nums[k + 1]; --k); // 跳过重复数字
+                }
+            }
+        }
+        return ans;
+    }
+}
