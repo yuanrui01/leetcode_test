d847f8ccb981a47f6024c935c847c078df360a5f
yuanrui01 <xdyrfree@gmail.com>
2023-03-19 23:43:08 +0800

    静态内部类实现懒汉式，懒加载，线程安全，不浪费空间，执行效率高

---
+++ src/main/java/org/example/Singleton5.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * description 静态内部类实现懒汉式，懒加载，线程安全，不浪费空间，执行效率高
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/19-23:40
+ */
+public class Singleton5 {
+    private Singleton5(){}
+
+    public static Singleton5 getInstance(){
+        return InnerClass.INSTANCE;
+    }
+
+    private static class InnerClass{
+        private final static Singleton5 INSTANCE = new Singleton5();
+    }
+}
