a702e7ab72c11cd6a4403d851191cff9417ec189
yuanrui01 <xdyrfree@gmail.com>
2023-03-19 23:45:36 +0800

    枚举单例 线程安全 懒加载 不浪费内存 效率高

---
+++ src/main/java/org/example/EnumSingleton.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * description 枚举单例 线程安全 懒加载 不浪费内存 效率高
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/19-23:43
+ */
+public enum EnumSingleton {
+
+    INSTANCE;
+
+    public void doSomething(String str){
+        System.out.println(str);
+    }
+}
