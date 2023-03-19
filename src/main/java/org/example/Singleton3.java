776e319aee2db749e95c3c8c70dfe3d4a9408888
yuanrui01 <xdyrfree@gmail.com>
2023-03-19 23:25:52 +0800

    懒汉式单例，线程安全，不会浪费内存，但是执行效率低

---
+++ src/main/java/org/example/Singleton3.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * description 懒汉式单例，线程安全，不会浪费内存，但是执行效率低
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/19-23:20
+ */
+public class Singleton3 {
+
+    private Singleton3(){}
+
+    private static Singleton3 instance;
+
+    public synchronized static  Singleton3 getInstance(){
+        if(instance == null){
+            instance =  new Singleton3();
+        }
+
+        return instance;
+    }
+}
