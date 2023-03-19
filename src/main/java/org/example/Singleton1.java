5093caa61a864202e4572607c5fdb50cb1e09cc6
yuanrui01 <xdyrfree@gmail.com>
2023-03-19 23:06:40 +0800

    饿汉式单例，线程安全，非懒加载，容易浪费内存空间，执行效率高

---
+++ src/main/java/org/example/Singleton1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * description 饿汉式单例，线程安全，非懒加载，容易浪费内存空间，执行效率高
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/19-23:04
+ */
+public class Singleton1 {
+
+    private Singleton1(){}
+    private static final Singleton1 instance = new Singleton1();
+
+    public static Singleton1 getInstance(){
+        return instance;
+    }
+}
