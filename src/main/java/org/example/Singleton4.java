75467568d7cc0404581ddf1fd30658497e33f257
yuanrui01 <xdyrfree@gmail.com>
2023-03-19 23:33:38 +0800

    双重锁检查，懒加载，线程安全，不浪费内存空间，执行效率高

---
+++ src/main/java/org/example/Singleton4.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/**
+ * description 双重锁检查，懒加载，线程安全，不浪费内存空间，执行效率高
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/19-23:30
+ */
+public class Singleton4 {
+
+    private Singleton4(){}
+
+    private static volatile Singleton4 instance;
+
+    public static Singleton4 getInstance(){
+        if(instance == null){
+            synchronized (Singleton4.class){
+                if(instance == null){
+                    instance = new Singleton4();
+                }
+            }
+        }
+
+        return instance;
+    }
+}
