ea678ac048343d793f46ae1d31b0f3d1167bf805
yuanrui01 <xdyrfree@gmail.com>
2023-03-19 17:33:28 +0800

    gctest1

---
+++ src/main/java/org/example/GCTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/19-13:43
+ */
+public class GCTest {
+    public static void main(String[] args) {
+        byte[] allocation1, allocation2;
+        allocation1 = new byte[30900*1024];
+        allocation2 = new byte[900*1024];
+
+    }
+}
+
