464dcb17a620753549196426448499e466147780
yuanrui01 <xdyrfree@gmail.com>
2023-03-19 23:03:01 +0800

    类加载器行为验证

---
+++ src/main/java/org/example/ClassLoaderTest1.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Arrays;
+
+/**
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/19-18:09
+ */
+public class ClassLoaderTest1 {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{1,2,3,4,6};
+
+        System.out.println(Arrays.toString(a));
+
+        System.out.println(a.getClass().getClassLoader());
+
+        A a1 = new A();
+
+        System.out.println(a1.getClass().getClassLoader());
+    }
+
+    private static class A{
+
+    }
+}
