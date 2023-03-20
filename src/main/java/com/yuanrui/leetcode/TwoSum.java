e28887d6d063eb346d3192ff827b577ad1c02410
yuanrui01 <xdyrfree@gmail.com>
2023-03-21 00:37:40 +0800

    1. 两数之和，补充O(N)解法

--- src/main/java/com/yuanrui/leetcode/TwoSum.java
+++ src/main/java/com/yuanrui/leetcode/TwoSum.java
@@ -37,4 +37,22 @@ public class TwoSum {
         res[1] = rear;
         return res;
     }
+
+    /**
+     * O(N)解法
+     */
+
+/*    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; ++i){
+            if(map.containsKey(Integer.valueOf(target - nums[i]))){
+                return new int[]{i,map.get(target - nums[i])};
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+
+        return new int[0];
+    }*/
 }
