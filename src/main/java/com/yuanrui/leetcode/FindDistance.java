2dde02359d6cb75825edfc370d15ccbb25e88e33
yuanrui01 <xdyrfree@gmail.com>
2023-03-23 02:13:59 +0800

    1740. 找到二叉树中的距离

---
+++ src/main/java/com/yuanrui/leetcode/FindDistance.java
@@ -0,0 +1,47 @@
+package com.yuanrui.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/22-23:34
+ */
+public class FindDistance {
+
+    public int findDistance(TreeNode root, int p, int q) {
+
+    }
+
+
+    /**
+     *
+     */
+    public List<Integer> writePath(TreeNode root, int target){
+        List<Integer> path = new ArrayList<>();
+        Stack<Integer> path = new Stack<>();
+
+
+        return path;
+    }
+
+    private boolean dfs(TreeNode root, int target, Stack<Integer> path){
+        if(root == null){
+            return false;
+        }
+        if(root.val ==  target){
+            path.push(target);
+            return true;
+        }else{
+            path.push(root.val);
+            if(root.left != null){
+                dfs(root.left,target,path);
+            }
+            if(root.left != null){
+                dfs(root.left,target,path);
+            }
+        }
+    }
+}
