f32e1af9556098a146440465835f5338256ab51b
yuanrui01 <xdyrfree@gmail.com>
2023-03-22 23:34:29 +0800

    放弃做题的一天！

---
+++ src/main/java/com/yuanrui/leetcode/FindPeakElement.java
@@ -0,0 +1,15 @@
+package com.yuanrui.leetcode;
+
+/**
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/3/22-11:31
+ */
+public class FindPeakElement {
+
+    public int findPeakElement(int[] nums) {
+
+        //TODO 太难了，下次做
+        return 0;
+    }
+}
