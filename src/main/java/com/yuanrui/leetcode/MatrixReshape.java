17516cf55345b0bfa71feaf35c83b208b744ab03
yuanrui01 <xdyrfree@gmail.com>
2023-03-31 00:33:05 +0800

    566. 重塑矩阵 补充注释

--- src/main/java/com/yuanrui/leetcode/MatrixReshape.java
+++ src/main/java/com/yuanrui/leetcode/MatrixReshape.java
@@ -8,28 +8,29 @@ package com.yuanrui.leetcode;
 public class MatrixReshape {
 
     public static void main(String[] args) {
-        matrixReshape(new int[][]{{1,2,3,4}},2,2);
+        matrixReshape(new int[][]{{1},{2},{3},{4}},2,2);
     }
 
-    public static int[][] matrixReshape(int[][] mat, int r, int c) {
-        int m = mat.length;
-        int n = mat[0].length;
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int mat_r = mat.length;
+        int mat_c = mat[0].length;
 
-        if(m*n != r*c){
+        //条件不满足直接返回
+        if(mat_r*mat_c != r*c){
             return mat;
-        }else{
-            int[][] res = new int[r][c];
-
-            int k = 0;
-
-            for(int i = 0; i < r; ++i){
-                for(int j = 0; j < c; ++j){
-                    res[i][j] = mat[k/n][k%n];
-                    ++k;
-                }
-            }
-
-            return res;
         }
+
+        //新数组
+        int[][] newArr = new int[r][c];
+
+        //注意后面算坐标都是通过总数和列数来算的
+        for(int i = 1; i <= mat_r; ++i){
+            for(int j = 1; j <= mat_c; ++j){
+                int t = (i-1)*mat_c + j;
+                newArr[(t - 1)/c][(t - 1)%c] = mat[i-1][j-1];
+            }
+        }
+
+        return newArr;
     }
 }
--- src/main/java/com/yuanrui/leetcode/MaxSubArray.java
+++ src/main/java/com/yuanrui/leetcode/MaxSubArray.java
@@ -1,11 +1,17 @@
 package com.yuanrui.leetcode;
 
+import java.util.*;
+
 /**
  * @author yuanrui
  * @email xdyrfree@gmail.com
  * @date 2023/3/25-0:17
  */
 public class MaxSubArray {
+
+    public static void main(String[] args) {
+        test();
+    }
 
     public int maxSubArray(int[] nums) {
         int ans = Integer.MIN_VALUE;
@@ -18,5 +24,31 @@ public class MaxSubArray {
             }
         }
         return ans;
+    }
+
+    public static void test(){
+        int[] src = new int[0];
+        int[] temp = Arrays.copyOf(src, src.length);
+        System.out.println(src.length);
+    }
+
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int[] nums2Count = new int[1001];
+        List<Integer> ilst = new ArrayList<>();
+
+
+        for(int i = 0; i < nums2.length; ++i){
+            nums2Count[nums2[i]]++;
+        }
+
+        for(int i = 0; i < nums1.length; ++i){
+            if(nums2Count[nums1[i]] > 0){
+                ilst.add(nums1[i]);
+                nums2Count[nums1[i]]--;
+            }
+        }
+
+        return ilst.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
