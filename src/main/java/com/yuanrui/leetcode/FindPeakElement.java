1c23e0c21609ba8150dc7a01f277191f384e91a6
yuanrui01 <xdyrfree@gmail.com>
2023-04-02 14:09:39 +0800

    162. 寻找峰值

--- src/main/java/com/yuanrui/leetcode/FindPeakElement.java
+++ src/main/java/com/yuanrui/leetcode/FindPeakElement.java
@@ -8,8 +8,39 @@ package com.yuanrui.leetcode;
 public class FindPeakElement {
 
     public int findPeakElement(int[] nums) {
+        int left = -1, right = nums.length - 1; // 开区间 (-1, n-1)
+        while (left + 1 < right) { // 开区间不为空
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) right = mid; // 蓝色
+            else left = mid; // 红色
+        }
+        return right;
+    }
 
-        //TODO 太难了，下次做
-        return 0;
+    public int findPeakElement2(int[] nums) {
+        int len = nums.length;
+        if(len == 1){
+            return 0;
+        }else if(nums[0] > nums[1]){
+            return 0;
+        }else if(nums[len - 1] > nums[len - 2]){
+            return len - 1;
+        }else{
+            int begin = 1;
+            int end = len - 2;
+
+            while(begin <= end){
+                int mid = (begin + end) / 2;
+                if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]){
+                    return mid;
+                }else if(nums[mid] < nums[mid + 1]){
+                    begin = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
     }
 }
