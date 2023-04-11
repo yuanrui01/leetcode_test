727f80e9710011e9321dd94741a809c68e2113e0
yuanrui01 <xdyrfree@gmail.com>
2023-04-11 22:00:24 +0800

    986. 区间列表的交集 -- 代码优化

--- src/main/java/com/yuanrui/leetcode/IntervalIntersection.java
+++ src/main/java/com/yuanrui/leetcode/IntervalIntersection.java
@@ -17,12 +17,10 @@ public class IntervalIntersection {
         //两个区间列表都有值
         int i = 0;
         int j = 0;
-
-        List<List<Integer>> ill = new ArrayList<>();
+        List<int[]> ans = new ArrayList<>();
         while(i != firstList.length && j != secondList.length){
-            List<Integer> res = interval(firstList[i], secondList[j]);
-            if (!res.isEmpty())
-                ill.add(res);
+            if (!(firstList[i][0] > secondList[j][1] || secondList[j][0] > firstList[i][1]))
+                ans.add(new int[]{Math.max(firstList[i][0], secondList[j][0]), Math.min(firstList[i][1], secondList[j][1])});
             //不同区间进行比较，小的那个+1
             if (firstList[i][1] > secondList[j][1])
                 j++;
@@ -30,25 +28,6 @@ public class IntervalIntersection {
                 i++;
         }
 
-        int[][] ans = new int[ill.size()][];
-        for (int k = 0; k < ill.size(); ++k){
-            ans[k] = new int[]{ill.get(k).get(0), ill.get(k).get(1)};
-        }
-        return ans;
-    }
-
-    /**
-     * 求两段区间的交集
-     * @param il1
-     * @param il2
-     * @return
-     */
-    private List<Integer> interval(int[] il1, int[] il2){
-        List<Integer> ans = new ArrayList<>();
-        if (!(il1[0] > il2[1] || il2[0] > il1[1])){
-            ans.add(Math.max(il1[0], il2[0]));
-            ans.add(Math.min(il1[1], il2[1]));
-        }
-        return ans;
+        return ans.toArray(new int[ans.size()][]);
     }
 }
