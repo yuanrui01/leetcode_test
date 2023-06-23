c2c96f4820595752267ff8128e8e53e05cdb81e9
yuanrui <xdyrfree@gmail.com>
2023-06-23 09:24:52 +0800

    117. 填充每个节点的下一个右侧节点指针 II

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * @author yuanrui
  * @email xdyrfree@gmail.com
@@ -7,6 +10,16 @@ package org.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> list = new ArrayList<>();
+        Set<String> sSet = new HashSet<>();
+        sSet = list.stream()
+            .collect(
+                Collectors.collectingAndThen(
+                    Collectors.toCollection(
+                        () -> new TreeSet<>( Comparator.comparing( s -> s.length() ) )
+                    )
+                    , HashSet::new
+                )
+        );
     }
 }
