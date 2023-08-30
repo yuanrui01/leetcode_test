453fd4526656f3f1534a9a714d7b10babd9f6d54
yuanrui <xdyrfree@gmail.com>
2023-08-30 09:44:29 +0800

    209. 长度最小的子数组 - 更新备注

--- src/main/java/com/yuanrui/leetcode/MinSubArrayLen.java
+++ src/main/java/com/yuanrui/leetcode/MinSubArrayLen.java
@@ -1,6 +1,7 @@
 package com.yuanrui.leetcode;
 
 /**
+ * 同向双指针例题：209. 长度最小的子数组
  * @author yuanrui
  * @email xdyrfree@gmail.com
  * @date 2023/3/12-0:27
@@ -8,19 +9,20 @@ package com.yuanrui.leetcode;
 public class MinSubArrayLen {
 
     public int minSubArrayLen(int target, int[] nums) {
-        int length = nums.length;
         int right = 0;
-        int ans = 1000001;
+        int ans = Integer.MAX_VALUE;
         int left = 0;
         int sum = 0;
-        for(right = 0; right < length; ++right){
+        for(right = 0; right < nums.length; ++right){
             sum += nums[right];
-
             while(sum > target){
                 ans = Math.min(ans, right - left + 1);
                 sum -= nums[left++];
             }
+            // 如果有答案，1就是最小的
+            if (ans == 1)
+                return ans;
         }
-        return ans == 1000001 ? 0 : ans;
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
