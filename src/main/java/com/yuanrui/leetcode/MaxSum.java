be2d15fa57d53744cb26ddf396c0deae55a352ac
yuanrui01 <xdyrfree@gmail.com>
2023-09-03 00:44:21 +0800

    refine code

--- src/main/java/com/yuanrui/leetcode/MaxSum.java
+++ src/main/java/com/yuanrui/leetcode/MaxSum.java
@@ -24,43 +24,26 @@ public class MaxSum {
 
     public static long maxSum2(List<Integer> nums, int m, int k) {
         long sum = 0L;
-        if (k == 1) {
-            for (Integer num : nums) {
-                sum = Math.max(num, sum);
-            }
-            return sum;
-        }
-
         int[] array = nums.stream().mapToInt(i -> i).toArray();
-
-        //保存上一个相同元素的索引
-        if (array.length == k) {
-            long tmpSum = 0L;
-            Set<Integer> set = new HashSet<>();
-            for (int i : array) {
-                set.add(i);
-                tmpSum += i;
-            }
-            if (set.size() >= m)
-                sum = Math.max(sum, tmpSum);
-        } else {
-            Map<Integer, Integer> map = new HashMap<>();
-            long tmpSum = 0L;
-            for (int i = 0; i < k; ++i) {
-                tmpSum += array[i];
-                map.merge(array[i], 1, Integer::sum);
-            }
-            if (map.keySet().size() >= m)
-                sum = tmpSum;
-            for(int i = 1; i <= array.length - k; i+=1) {
-                tmpSum -= array[i-1];
-                tmpSum += array[i+k-1];
-                if (map.containsKey(array[i-1]) && map.get(array[i-1]) == 1)
+        Map<Integer, Integer> map = new HashMap<>();
+        long tmpSum = 0L;
+        for (int i = 0; i < k; ++i) {
+            tmpSum += array[i];
+            map.merge(array[i], 1, Integer::sum);
+        }
+        if (map.keySet().size() >= m)
+            sum = tmpSum;
+        for(int i = 1; i <= array.length - k; i+=1) {
+            tmpSum -= array[i-1];
+            tmpSum += array[i+k-1];
+            if (map.containsKey(array[i-1]))
+                if (map.get(array[i-1]) == 1)
                     map.remove(array[i-1]);
-                map.merge(array[i+k-1], 1, Integer::sum);
-                if (map.keySet().size() >= m)
-                    sum = Math.max(sum, tmpSum);
-            }
+                else
+                    map.merge(array[i+k-1], -1, Integer::sum);
+            map.merge(array[i+k-1], 1, Integer::sum);
+            if (map.keySet().size() >= m)
+                sum = Math.max(sum, tmpSum);
         }
         return sum;
     }
