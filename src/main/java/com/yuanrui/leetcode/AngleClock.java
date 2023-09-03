10a4f6264849798d7cccbc9451f9575afa9bec54
yuanrui01 <xdyrfree@gmail.com>
2023-09-04 01:19:40 +0800

    1344. 时钟指针的夹角

---
+++ src/main/java/com/yuanrui/leetcode/AngleClock.java
@@ -0,0 +1,21 @@
+package com.yuanrui.leetcode;
+
+/**
+ * @author: yuanrui
+ */
+public class AngleClock {
+
+    public static void main(String[] args) {
+        System.out.println(angleClock(12,30));
+    }
+
+    public static double angleClock(int hour, int minutes) {
+        double hourAngle = (hour%12) * 30.0 + 30.0*minutes/60;
+        double minuteAngle = minutes*6;
+
+        double includeAngle = Math.abs(minuteAngle - hourAngle);
+
+        return Math.min(includeAngle, 360-includeAngle);
+    }
+
+}
