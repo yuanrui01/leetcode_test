3077aa56cce68cfc00ec39940d75d769ad1146ae
yuanrui01 <xdyrfree@gmail.com>
2023-09-05 20:13:40 +0800

    2126. 摧毁小行星

--- src/main/java/com/yuanrui/leetcode/AsteroidsDestroyed.java
+++ src/main/java/com/yuanrui/leetcode/AsteroidsDestroyed.java
@@ -21,4 +21,28 @@ public class AsteroidsDestroyed {
         return true;
     }
 
+    public boolean asteroidsDestroyed2(int mass, int[] asteroids) {
+//        int[] sortArr = new int[100001];
+//        for (int i = 0; i < asteroids.length; ++i) {
+//            sortArr[asteroids[i]]++;
+//        }
+//        int k = 0;
+//        for (int i = 0; i < sortArr.length; i++) {
+//            while(sortArr[i] > 0) {
+//                asteroids[k++]=i;
+//                sortArr[i]--;
+//            }
+//        }
+//
+//        long massl = mass;
+//        for (int i = 0; i < asteroids.length; ++i) {
+//            if (massl >= asteroids[i])
+//                massl += asteroids[i];
+//            else
+//                return false;
+//        }
+//
+        return true;
+    }
+
 }
