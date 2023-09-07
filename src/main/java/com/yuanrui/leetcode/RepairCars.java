e7489f278a829b036f577ef8151c26aeb60b40ee
yuanrui01 <xdyrfree@gmail.com>
2023-09-07 23:18:08 +0800

    2594. 修车的最少时间

--- src/main/java/com/yuanrui/leetcode/RepairCars.java
+++ src/main/java/com/yuanrui/leetcode/RepairCars.java
@@ -1,6 +1,7 @@
 package com.yuanrui.leetcode;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * 2594. 修车的最少时间
@@ -44,10 +45,39 @@ public class RepairCars {
         return cache[idx][cars] = Math.min(ans, cache[idx-1][cars]);
     }
 
-    public static void main(String[] args) {
-        int[] ranks = {9,7,10,4,4,8,10,2,7,9,8,1,8,3,1,9,1};
-        int cars = 6977;
+    public static long repairCars2(int[] ranks, int cars) {
+        long ans = 0L;
 
-        System.out.println(repairCars(ranks, cars));
+        int min = Integer.MAX_VALUE;
+        for (int rank : ranks) {
+            min = Math.min(min, rank);
+        }
+
+
+        long left = 0, right = (long) min * cars * cars;
+        while (left + 1 < right) { // 区间不为空
+            long mid = (left + right) >>> 1;
+            if (check(ranks, mid, cars))
+                right = mid; // 范围缩小到 (mid, right)
+            else
+                left = mid; // 范围缩小到 (left, mid)
+        }
+        return right; // 或者 left+1
+    }
+
+    private static boolean check(int[] ranks, long t, int cars) {
+        long sum = 0L;
+        for (int rank : ranks) {
+            sum += Math.floor(Math.sqrt(t/rank));
+        }
+        return sum >= cars;
+
+    }
+
+    public static void main(String[] args) {
+        int[] ranks = {4,2,3,1};
+        int cars = 10;
+
+        System.out.println(repairCars2(ranks, cars));
     }
 }
