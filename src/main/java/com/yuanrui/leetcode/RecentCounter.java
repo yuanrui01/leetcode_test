183591115ace499a7bb7aec73ce9177b54b85022
yuanrui <xdyrfree@gmail.com>
2023-09-08 13:13:22 +0800

    933. 最近的请求次数

--- src/main/java/com/yuanrui/leetcode/RecentCounter.java
+++ src/main/java/com/yuanrui/leetcode/RecentCounter.java
@@ -1,6 +1,8 @@
 package com.yuanrui.leetcode;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,14 +11,20 @@ import java.util.List;
 public class RecentCounter {
 
     List<Integer> il;
-
     public RecentCounter() {
         il = new ArrayList<>();
     }
 
     public int ping(int t) {
+        Iterator<Integer> itr = il.iterator();
+        while (itr.hasNext()) {
+            Integer next = itr.next();
+            if (t - next > 3000)
+                itr.remove();
+            else
+                break;
+        }
         il.add(t);
-        il.removeIf(i-> t-i>3000);
         return il.size();
     }
 }
