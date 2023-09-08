f1b4cc3b2ad0a94eb6e78125346c9f2c832eb288
yuanrui <xdyrfree@gmail.com>
2023-09-08 12:09:26 +0800

    2161. 根据给定数字划分数组

---
+++ src/main/java/com/yuanrui/leetcode/PivotArray.java
@@ -0,0 +1,26 @@
+package com.yuanrui.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PivotArray {
+
+
+    public int[] pivotArray(int[] nums, int pivot) {
+        List<Integer> equals = new ArrayList<>();
+        List<Integer> small = new ArrayList<>();
+        List<Integer> big = new ArrayList<>();
+        for (int num : nums) {
+            if (num > pivot)
+                big.add(num);
+            else if (num < pivot)
+                small.add(num);
+            else
+                equals.add(num);
+        }
+        small.addAll(equals);
+        small.addAll(big);
+        return small.stream().mapToInt(i->i).toArray();
+    }
+}
