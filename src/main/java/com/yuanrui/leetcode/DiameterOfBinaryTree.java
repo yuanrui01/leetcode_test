7c7363c77421b7d0e11cd13cc79198ef3a6a9543
yuanrui01 <xdyrfree@gmail.com>
2023-09-09 22:15:11 +0800

    543. 二叉树的直径 - 优化完毕

--- src/main/java/com/yuanrui/leetcode/DiameterOfBinaryTree.java
+++ src/main/java/com/yuanrui/leetcode/DiameterOfBinaryTree.java
@@ -1,22 +1,28 @@
 package com.yuanrui.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 543. 二叉树的直径
  * @author: yuanrui
  */
 public class DiameterOfBinaryTree {
 
+    int max = 0;
     public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null)
-            return 0;
-        else
-            return Math.max(depth(root.left) + depth(root.right), Math.max(diameterOfBinaryTree(root.left),diameterOfBinaryTree(root.right)));
+        depth(root);
+        return max;
     }
 
     private int depth(TreeNode root) {
         if (root == null)
             return 0;
-        else
-            return Math.max(depth(root.left) + 1, depth(root.right) + 1);
+        else {
+            int leftDepth = depth(root.left);
+            int rightDepth = depth(root.right);
+            max = Math.max(max, leftDepth + rightDepth);
+            return Math.max(leftDepth, rightDepth) + 1;
+        }
     }
 }
