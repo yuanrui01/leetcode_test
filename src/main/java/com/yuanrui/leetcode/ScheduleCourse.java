dcbd63fd99656abc050aaac8cb8c548123950422
yuanrui01 <xdyrfree@gmail.com>
2023-09-15 03:44:09 +0800

    630. 课程表 III （优先队列 + 贪心）

--- src/main/java/com/yuanrui/leetcode/ScheduleCourse.java
+++ src/main/java/com/yuanrui/leetcode/ScheduleCourse.java
@@ -1,76 +1,29 @@
 package com.yuanrui.leetcode;
 
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
- * 630. 课程表 III
+ * 630. 课程表 III （优先队列 + 贪心）
  * @author: yuanrui
  */
 public class ScheduleCourse {
-    int[][] courses;
-    int[][] cache;
-
     public int scheduleCourse(int[][] courses) {
-        int len = courses.length;
-        cache = new int[courses.length][10000];
-        Arrays.sort(courses, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1] - o2[1];
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]); // 按照 lastDay 从小到大排序
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a); // 最大堆
+        int day = 0; // 已消耗时间
+        for (int[] c : courses) {
+            int duration = c[0], lastDay = c[1];
+            if (day + duration <= lastDay) { // 没有超过 lastDay，直接学习
+                day += duration;
+                pq.offer(duration);
+            } else if (!pq.isEmpty() && duration < pq.peek()) { // 该课程的时间比之前的最长时间要短
+                // 反悔，撤销之前 duration 最长的课程，改为学习该课程
+                // 节省出来的时间，能在后面上完更多的课程
+                day -= pq.poll() - duration;
+                pq.offer(duration);
             }
-        });
-
-        for (int[] ints : cache) {
-            Arrays.fill(ints,-1);
         }
-
-        this.courses = courses;
-        return dp(len - 1, courses[len - 1][1]);
-    }
-
-    /**
-     * 在给定剩余时间内，能从（1，idx）中最多选择多少门课程
-     * @param idx
-     * @return
-     */
-    private int dp(int idx, int endTime) {
-        if (idx == 0) {
-            if (endTime >= courses[0][0])
-                return 1;
-            else
-                return 0;
-        }
-        if (cache[idx][endTime] != -1)
-            return cache[idx][endTime];
-        // 没时间选了
-        if (courses[idx][0] > endTime) {
-            return cache[idx][endTime] = dp(idx-1, Math.min(endTime, courses[idx-1][1]));
-        }
-        int beginTime = endTime - courses[idx][0];
-        // 可以选，也可以不选，返回选或不选的最大值
-        return cache[idx][endTime] = Math.max(dp(idx-1, Math.min(beginTime, courses[idx - 1][1])) + 1,
-                dp(idx-1, Math.min(endTime, courses[idx-1][1])));
-    }
-
-
-    public static void main(String[] args) {
-//        int[][] courses = {{100, 200}, {200, 1300}, {1000, 1250}, {2000, 3200}};
-        int[][] courses = {{7,16},{2,3},{3,12},{3,14},{10,19},{10,16},{6,8},{6,11},{3,13},{6,16}};
-        int[][] courses1 = {{1, 2}};
-        int[][] courses2 = {{3,2}, {4,3}};
-        int[][] courses3 = {{5,5}, {4,6},{2,6}};
-//        Arrays.sort(courses, new Comparator<int[]>() {
-//            @Override
-//            public int compare(int[] o1, int[] o2) {
-//                return o1[1] - o2[1];
-//            }
-//        });
-
-        ScheduleCourse scheduleCourse = new ScheduleCourse();
-
-        System.out.println(scheduleCourse.scheduleCourse(courses));
-//        System.out.println(scheduleCourse.scheduleCourse(courses1));
-//        System.out.println(scheduleCourse.scheduleCourse(courses2));
+        return pq.size();
     }
 }
