c3a845374bec236ee8dc08b0d195f630d7a5e6be
yuanrui <xdyrfree@gmail.com>
2023-09-15 17:24:46 +0800

    1237. 找出给定方程的正整数解

---
+++ src/main/java/com/yuanrui/leetcode/FindSolution.java
@@ -0,0 +1,29 @@
+package com.yuanrui.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 1237. 找出给定方程的正整数解
+ */
+public class FindSolution {
+
+    interface CustomFunction {
+         public int f(int x, int y);
+    };
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 1; i <= z; ++i)
+            for (int j = 1; j <= z; ++j)
+                if (customfunction.f(i,j) == z) {
+                    List<Integer> subAns = new ArrayList<>();
+                    subAns.add(i);
+                    subAns.add(j);
+                    ans.add(subAns);
+                }
+        return ans;
+    }
+}
