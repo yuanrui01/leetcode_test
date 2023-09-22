1f1b4bb5c385012fbf6acf2b5838a1be36c9dc1e
yuanrui <xdyrfree@gmail.com>
2023-09-22 12:18:14 +0800

    2. 两数相加

--- src/main/java/com/yuanrui/leetcode/AddTwoNumbers.java
+++ src/main/java/com/yuanrui/leetcode/AddTwoNumbers.java
@@ -2,53 +2,62 @@ package com.yuanrui.leetcode;
 
 import java.util.List;
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * 2. 两数相加
+ */
 public class AddTwoNumbers {
 
+    /**
+     * l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+     * [8,9,9,9,0,0,0,1]
+     * @param args
+     */
     public static void main(String[] args) {
         ListNode l1 = new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9)))))));
         ListNode l2 = new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9))));
-        addTwoNumbers(l1,l2);
+        ListNode l3 = addTwoNumbers(l1, l2);
+
+        System.out.println(l3);
+
+//        AtomicReference<ListNode> nodeRef = new AtomicReference<>();
+//
+//        System.out.println(nodeRef.get());
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode node = new ListNode(0);
-        ListNode head = node;
-
+        ListNode head = l1;
         int carry = 0;
-        int v1 = 0;
-        int v2 = 0;
-        while(l1 != null && l2 != null){
-            node.next = new ListNode(0);
-            node = node.next;
-            v1 = l1.val;
-            v2 = l2.val;
-            node.val = (v1 + v2 + carry) % 10;
+        ListNode pre = null;
+        while (l1 != null && l2!= null) {
+            pre = l1;
+            int v1 = l1.val;
+            l1.val = (v1 + l2.val + carry)%10;
+            carry = (v1 + l2.val + carry)/10;
             l1 = l1.next;
             l2 = l2.next;
-            carry = (v1 + v2 + carry) / 10;
+            if (l1 == null && l2 == null) {
+                if (carry > 0)
+                    pre.next = new ListNode(carry);
+                return head;
+            }
         }
-
-        while (l1 != null){
-            node.next = new ListNode(0);
-            node = node.next;
-            node.val = (l1.val + carry) % 10;
-            carry = (l1.val + carry) / 10;
-            l1 = l1.next;
+        if (l1 == null) {
+            pre.next = l2;
         }
-
-        while (l2 != null){
-            node.next = new ListNode(0);
-            node = node.next;
-            node.val = (l2.val + carry) % 10;
-            carry = (l2.val + carry) / 10;
-            l2 = l2.next;
+        pre = pre.next;
+        while (pre != null) {
+            int v = pre.val;
+            pre.val = (v + carry)%10;
+            carry = (v + carry)/10;
+            if (pre.next == null && carry > 0) {
+                pre.next = new ListNode(carry);
+                break;
+            }
+            pre = pre.next;
         }
-
-        if(carry != 0){
-            node.next = new ListNode(carry);
-        }
-        return head.next;
+        return head;
     }
 }
 
