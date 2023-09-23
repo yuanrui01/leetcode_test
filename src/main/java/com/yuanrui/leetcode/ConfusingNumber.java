f7cd5d4b21ace8eddebe55b261bc1f5c23cadd8b
yuanrui01 <xdyrfree@gmail.com>
2023-09-23 09:06:44 +0800

    1056. 易混淆数

--- src/main/java/com/yuanrui/leetcode/ConfusingNumber.java
+++ src/main/java/com/yuanrui/leetcode/ConfusingNumber.java
@@ -5,15 +5,15 @@ package com.yuanrui.leetcode;
  * @author: yuanrui
  */
 public class ConfusingNumber {
-    int[] rotateArr = {0,1,-1,-1,-1,-1,9,-1,8,6};
+    int[] rotateArr = {0, 1, -1, -1, -1, -1, 9, -1, 8, 6};
     public boolean confusingNumber(int n) {
         String str = String.valueOf(n);
         StringBuilder rotateStr = new StringBuilder();
         for (int i = 0; i < str.length(); ++i)
-            if (rotateArr[str.charAt(i)-'0'] == -1)
+            if (rotateArr[str.charAt(i) - '0'] == -1)
                 return false;
             else
-                rotateStr.append(rotateArr[str.charAt(i)-'0']);
+                rotateStr.append(rotateArr[str.charAt(i) - '0']);
         return !str.contentEquals(rotateStr.reverse());
     }
 }
