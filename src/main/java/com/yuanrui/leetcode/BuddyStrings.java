46fc90d4df832ad07fd7f9c36af0418354b7bd4b
yuanrui01 <xdyrfree@gmail.com>
2023-09-23 09:25:04 +0800

    859. 亲密字符串

--- src/main/java/com/yuanrui/leetcode/BuddyStrings.java
+++ src/main/java/com/yuanrui/leetcode/BuddyStrings.java
@@ -11,34 +11,31 @@ public class BuddyStrings {
     }
 
     public static boolean buddyStrings(String s, String goal) {
-        if (s.length() != goal.length()) {
+        // 0. 不必多说
+        if (s.length() != goal.length())
             return false;
-        }
-
+        int[] sArr = new int[26];
+        // 1. 字符串相等，仅当有重复元素时才为真
         if (s.equals(goal)) {
-            int[] count = new int[26];
-            for (int i = 0; i < s.length(); i++) {
-                count[s.charAt(i) - 'a']++;
-                if (count[s.charAt(i) - 'a'] > 1) {
+            for (int i = 0; i < s.length(); ++i)
+                if (++sArr[s.charAt(i) - 'a'] > 1)
                     return true;
-                }
-            }
             return false;
         } else {
-            int first = -1, second = -1;
-            for (int i = 0; i < goal.length(); i++) {
+            // 2. 字符串不相等，那么仅当字符差距为2，且交叉相等才为真
+            int diff = 0;
+            int first = -1;
+            int second = -1;
+            for (int i = 0; i < s.length(); ++i)
                 if (s.charAt(i) != goal.charAt(i)) {
-                    if (first == -1)
-                        first = i;
-                    else if (second == -1)
-                        second = i;
-                    else
+                    diff++;
+                    second = first;
+                    first = i;
+                    if (diff > 2)
                         return false;
                 }
-            }
-
-            return (second != -1 && s.charAt(first) == goal.charAt(second) &&
-                    s.charAt(second) == goal.charAt(first));
+            return diff == 2 &&
+                    s.charAt(first) == goal.charAt(second) && s.charAt(second) == goal.charAt(first);
         }
     }
 }
