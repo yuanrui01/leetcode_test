55cb2b3a85cb98a38b31b8cc34f4c15e6a928c2c
yuanrui <xdyrfree@gmail.com>
2023-09-25 11:58:13 +0800

    65. 有效数字

--- src/main/java/com/yuanrui/leetcode/IsNumber.java
+++ src/main/java/com/yuanrui/leetcode/IsNumber.java
@@ -8,23 +8,27 @@ public class IsNumber {
 
     public static void main(String[] args) {
         IsNumber isNumber = new IsNumber();
-
-        System.out.println(isNumber.isNumber(".1"));
+        System.out.println(isNumber.isNumber("+.E3"));
     }
 
     public boolean isNumber(String s) {
+        if (s.equals("."))
+            return false;
         s = s.toLowerCase();
         int eIdx = s.indexOf('e');
         if (eIdx > -1) {
-            return eIdx != 0 && isIntegerOfFloat(s.substring(0, eIdx)) && isInteger(s.substring(eIdx));
+            return eIdx != 0 && isIntegerOfFloat(s.substring(0, eIdx)) && isInteger(s.substring(eIdx+1));
         }
         int dotIdx = s.indexOf('.');
         if (dotIdx > -1) {
-            if (dotIdx == (s.length() - 1))
+            if (dotIdx < s.length() - 1 && (s.charAt(dotIdx+1) == '-' || s.charAt(dotIdx+1) == '+'))
                 return false;
-            else if (dotIdx == 0) {
+            else if (dotIdx == 0)
                 return postDot(s.substring(dotIdx+1));
-            } else return preDot(s.substring(0, dotIdx)) && postDot(s.substring(dotIdx + 1));
+            else if (dotIdx == s.length() - 1)
+                return isInteger(s.substring(0,dotIdx));
+            else
+                return preDot(s.substring(0, dotIdx)) && postDot(s.substring(dotIdx + 1));
         }
 
 
@@ -41,6 +45,8 @@ public class IsNumber {
     }
 
     private boolean postDot(String s) {
+        if (s.isEmpty())
+            return true;
         return isPositiveInteger(s);
     }
 
@@ -75,11 +81,19 @@ public class IsNumber {
             return false;
         if (s.charAt(0) == '-' || s.charAt(0) == '+')
             s = s.substring(1);
+        if (s.equals("."))
+            return false;
         if (s.isEmpty())
             return false;
-        for (int i = 0; i < s.length(); i++)
-            if (!((s.charAt(i) >= '0' && s.charAt(i) <= '9') || s.charAt(i) == '.'))
+        int countDot = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '.') {
+                countDot++;
+                continue;
+            }
+            if (!(s.charAt(i) >= '0' && s.charAt(i) <= '9'))
                 return false;
-        return true;
+        }
+        return countDot <= 1;
     }
 }
