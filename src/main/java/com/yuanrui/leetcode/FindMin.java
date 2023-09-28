624709b8115e8722618051a2c55d3fa56846cf31
yuanrui <xdyrfree@gmail.com>
2023-09-28 17:08:45 +0800

    153. 寻找旋转排序数组中的最小值 154. 寻找旋转排序数组中的最小值 II

--- src/main/java/com/yuanrui/leetcode/FindMin.java
+++ src/main/java/com/yuanrui/leetcode/FindMin.java
@@ -1,29 +1,56 @@
 package com.yuanrui.leetcode;
 
 
+import java.util.Collections;
+
 /**
  * 153. 寻找旋转排序数组中的最小值
+ * 154. 寻找旋转排序数组中的最小值 II
  */
 public class FindMin {
 
-    public int findMin(int[] nums) {
+    public int findMinRight(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
+        int minIdx= nums.length - 1;
         int middle;
         while (left <= right) {
             middle = (left + right) >>> 1;
-            if (nums[middle] <= nums[nums.length - 1])
+            if (nums[middle] < nums[minIdx]) {
                 right = middle - 1;
-            else
+                minIdx = middle;
+            } else if (nums[middle] > nums[nums.length - 1]){
                 left = middle + 1;
+            } else {
+                right = right - 1;
+            }
         }
-        return nums[left];
+        return nums[minIdx];
+    }
+
+    public int findMinLeft(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int minIdx= nums.length - 1;
+        int middle;
+        while (left <= right) {
+            middle = (left + right) >>> 1;
+            if (nums[middle] < nums[minIdx]) {
+                right = middle - 1;
+                minIdx = middle;
+            } else if (nums[middle] > nums[nums.length - 1]){
+                left = middle + 1;
+            } else {
+                left = left + 1;
+            }
+        }
+        return nums[minIdx];
     }
 
 
     public static void main(String[] args) {
         FindMin findMin = new FindMin();
-        int[] nums = {3,4,5,1,1,1,2,3};
-        System.out.println(findMin.findMin(nums));
+        int[] nums = {2,2,2,0,2,2};
+        System.out.println(Math.min(findMin.findMinRight(nums), findMin.findMinLeft(nums)));
     }
 }
