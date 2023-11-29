7d66d5aaa287faf8c93437234278c3b7f9a13842
yuanrui <xdyrfree@gmail.com>
2023-11-29 16:16:42 +0800

    Refactor HIndex2 algorithm and modify test input
    This commit refactors the HIndex2 algorithm in /src/main/java/com/yuanrui/leetcode/HIndex2.java file to enhance its performance and readability. The binary search part is rewritten, integrating Math.min and additional statements to find the largest h-index value more accurately and efficiently. In detail, more variables `middleH`, `middlePlusH`, `lastHIndex`, `hIndex` are introduced for clearer logic and less repetition in calculations. Also, the test input in the main method has been adjusted to give a more representative example of a range of potential citation counts.

--- src/main/java/com/yuanrui/leetcode/HIndex2.java
+++ src/main/java/com/yuanrui/leetcode/HIndex2.java
@@ -5,24 +5,26 @@ public class HIndex2 {
     public int hIndex(int[] citations) {
         int size = citations.length;
         int left = 0;
-        int right = citations.length - 1;
-        if (citations[0] >= size)
-            return size;
-
-        while(left < right) {
-            int middle = (left + right) >>> 1;
-            if ((size - middle) < citations[middle]) {
-                right = middle - 1;
-            } else {
+        int right = citations.length - 2;
+        // 理解为求极大值点即可 --- Math.min(size - middle, citations[middle])
+        while(left <= right) {
+            int middle = (left + right) / 2;
+            int middleH = Math.min(size - middle, citations[middle]);
+            int middlePlusH = Math.min(size - middle - 1, citations[middle + 1]);
+            if (middleH <= middlePlusH) {
                 left = middle + 1;
+            } else {
+                right = middle - 1;
             }
         }
-        return left == 0 ? 0 : Math.min(right, citations[right]);
+        int lastHIndex = Math.min(1, citations[size - 1]);
+        int hIndex = Math.min(size - left, citations[left]);
+        return Math.max(hIndex, lastHIndex);
     }
 
     public static void main(String[] args) {
         HIndex2 hIndex2 = new HIndex2();
-        int[] citations = {0, 100};
+        int[] citations = {0,1,3,5,6};
         System.out.println(hIndex2.hIndex(citations));
     }
 }
