f360adeb8957971b0dd9713a3998ca43b1e227e1
yuanrui <xdyrfree@gmail.com>
2024-03-28 09:46:13 +0800

    915. 分割数组

--- src/main/java/com/yuanrui/fifteen/PartitionDisjoint.java
+++ src/main/java/com/yuanrui/fifteen/PartitionDisjoint.java
@@ -7,24 +7,20 @@ public class PartitionDisjoint {
 
     public int partitionDisjoint(int[] nums) {
         int length = nums.length;
-        int[] leftStat = new int[length];
         int[] rightStat = new int[length];
-
-        // 对于leftStat每个值表示从左往右累计的最大值
-        leftStat[0] = nums[0];
-        for (int i = 1; i < length - 1; i++)
-            leftStat[i] = Math.max(nums[i], leftStat[i - 1]);
 
         // 对于rightStat每个值表示从右往左累计的最小值
         rightStat[length - 1] = nums[length - 1];
         for (int j = length - 2; j >= 1; --j )
             rightStat[j] = Math.min(nums[j], rightStat[j + 1]);
 
+        int leftMax = nums[0];
         for (int i = 0; i < length - 1; ++i) {
-            if (leftStat[i] < rightStat[i+1])
+            if (leftMax <= rightStat[i+1])
                 return i + 1;
+            leftMax = Math.max(leftMax, nums[i]);
         }
-        return 0;
+        return 886;
     }
 
     public static void main(String[] args) {
